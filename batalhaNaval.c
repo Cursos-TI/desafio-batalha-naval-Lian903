bb9de65c8d4b02ad835011322ce822e96e0240a9
Lian903 <smouche463@gmail.com>
2025-04-11 18:42:48 +0000

    // Commit do 11/04: Declarado estruturas de repetição e mais

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,3 +1,7 @@
+
+#define LINHA 10
+#define COLUNA 11
+#define TAM_P 3 // Definido a quantidade de elementos para o vetor
 #include <stdio.h>
 
 // Desafio Batalha Naval - MateCheck
@@ -5,32 +9,60 @@
 // Siga os comentários para implementar cada parte do desafio.
 
 int main() {
-    // Nível Novato - Posicionamento dos Navios
-    // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
-    // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
-    // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
+    // Nível Novato - Posicionamento dos Navios = [100%]
 
-    int tabuleiro[9][9] = 
-    {
-    {1,0,0,0,0,0,0,0,0,0,0},
-    {2,0,0,3,0,0,0,0,0,0,0},
-    {3,0,0,3,0,0,0,0,0,0,0},
-    {4,0,0,3,0,0,0,0,0,0,0},
-    {5,0,0,0,0,0,0,0,0,0,0},
-    {6,0,0,0,0,0,0,0,0,0,0},
-    {7,0,0,0,0,0,0,0,0,0,0},
-    {8,0,0,3,3,3,0,0,0,0,0},
-    {9,0,0,0,0,0,0,0,0,0,0},
-    };
+    // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal = [100%]
     
+    int Matriz[LINHA][COLUNA];
+    int i, j;
+    int Navio[TAM_P];
     char letras;
+    int numeros[LINHA];
+    //Processo Passo a Passo:
     
-    printf("Tabuleiro de Batalha Naval!\n");
+    // Passo 1: Declarei o Vetor
+    for (i = 0; i < TAM_P; i++)
+    {
+        Navio[i] = 3; 
+    }
+    
+    //Passo opcional: Numeração declarada
+    for (i = 0; i < LINHA; i++)
+    {
+        numeros[i] = 1 + i; // Ira imprimir as casas de 1 a 10
+    }
+    
+    // Passo 2: Declarei a Matriz como zeros
+    for (i = 0; i < LINHA; i++)
+    {
+        for (j = 0; j < COLUNA; j++)
+        {
+            Matriz[i][j] = 0;
+        }
+        
+    }
+    
+    // Passo 3: Inseri o Vetor na Matriz 10x10
+    for (i = 0; i < TAM_P; i++)
+    {
+        Matriz[1][2 + i] = Navio[i]; //Aqui declara a coordenada do vetor e o modo como ele será exibido
+        Matriz[i][8] = Navio[i]; // Exemplo: Aqui ele será exibido na Vertical na Linha 1 (indice 0) e coluna 9 (indice 8)
+    }
+    
+    // Passo 3.5: Inseri o Vetor Diagonal Primária e Secundária na Matriz 10x10
+    for (i = 0; i < TAM_P; i++)
+    {
+        Matriz[6 - i][2 + i] = Navio[i]; // Sinais diferentes resultam numa Diagonal Secundaria
+        Matriz[6 + i][5 + i] = Navio[i]; // Sinais iguais resultam numa Digonal Primaria
+    }
+    
+    // Passo opcional: Exibir o titulo, letras e numeros
+    printf("Tabuleiro de Batalha Naval!\n"); 
     printf("   ");
-    for (int lin = 0; lin <= 0; lin++) // Este FOR serve para a qauntidades de linhas das letras
+    for (int lin = 0; lin <= 0; lin++) // Este for serve para a qauntidades de linhas para letras (Aqui será apenas uma linha)
     {
         letras = 'A';
-       for (int h = 0; h <= 7; h++)   // quantidades de letras
+       for (int h = 0; h <= 9; h++)   // quantidades de letras combina com o tabuleiro sendo 10 (indice 9)
        {
         printf("%c ", letras);
         letras++;
@@ -39,27 +71,25 @@ int main() {
     }
     printf("\n");
     
-    for (int j = 0; j <= 8; j++) // Quantidades de linhas 
+    for (i = 0; i < LINHA; i++)
     {
-    for (int i = 0; i <= 8; i++) // Quantidades de colunas
-    {
-        printf(" %d", tabuleiro[j][i]);
-    }
-    printf("\n");
+        Matriz[i][0] = numeros[i];
     }
     
-    printf("Nomeclatura das coordenadas:\n");
-    printf("Coordenadas que começam com um numero: Horizontal\n");
-    printf("Coordenadas que começam com um caractere: Vertical\n");
-    printf("Seu návio se encontra na coordenada: %dCDE \n", tabuleiro[7][0]);
-    printf("O návio Inimigo se encontra na coordenada: C234 \n");
+    // Passo 4: Imprime a Matriz com o vetor já inserido na mesma
+    for (i = 0; i < LINHA; i++)
+    {
+        for (j = 0; j < COLUNA; j++)
+        { 
+            printf(" %d", Matriz[i][j]);
+            
+        }
+        printf("\n");
+    }
     
-
-    // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
-    // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
-    // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
-    // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
-
+    return 0;
+    }
+    
     // Nível Mestre - Habilidades Especiais com Matrizes
     // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
     // Sugestão: Utilize estruturas de repetição aninhadas para preencher as áreas afetadas por essas habilidades no tabuleiro.
@@ -81,5 +111,4 @@ int main() {
     // 1 1 1 1 1
     // 0 0 1 0 0
 
-    return 0;
-}
+ 
